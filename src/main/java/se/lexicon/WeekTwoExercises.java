e5f99bd187d069ea43ac13da1e3943cf4235aa4d
leoodindealcantara <leoalcantara@icloud.com>
2021-06-07 11:24:24 +0200

    Week 2 projects

--- src/main/java/se/lexicon/WeekTwoExercises.java
+++ src/main/java/se/lexicon/WeekTwoExercises.java
@@ -8,7 +8,7 @@ public class WeekTwoExercises {
 
 
     //Exercise 02
-    public static int indexOf(int findIndexElement, int source[]) {
+   /* public static int indexOf(int findIndexElement, int source[]) {
         int indexFound = Arrays.binarySearch(source, findIndexElement);
 
         if (indexFound >= 0) {
@@ -16,62 +16,57 @@ public class WeekTwoExercises {
         } else {
             return -1;
         }
-    }
-
-
-
-
-
-
-
-    //Exercise 08
-    /*public static int[] removeDuplicates() {
-        int[] numbers = {20, 20, 40, 20, 30, 40, 50, 60, 50};
-        Arrays.sort(numbers);
-        System.out.println(Arrays.toString(numbers));
-        int[] duplicatedElements = {};
-        for (int i = 0; i < numbers.length; i++) {
-            for (int j = i + 1; j < numbers.length; j++) {
-                if (numbers[i] == numbers[j]);
-                }
-        }
-        return numbers;
     }*/
 
-    public static int removeDuplicateElements(int numbers[], int n){
-        if (n==0 || n==1){
+
+
+
+
+
+    //Exercise 8
+    // parameter of source
+    // return array without duplicates
+    // sort
+    //prep empty array
+    // are they the same
+    // if not duplicate use method add element
+    // return array with no dup
+
+    public static int removeDuplicatedElements(int numbers[], int n){
+        if(n==0 || n==1){
             return n;
         }
-        int[] duplicatedNumbers = new int[n];
+        int[] temp = new int[n];
         int j = 0;
         for (int i=0; i<n-1; i++){
-            if (numbers[i] != numbers[i+1]){
-                duplicatedNumbers[j++] = numbers[i];
+            if(numbers[i] != numbers[i+1]){
+                temp[j++] = numbers[i];
             }
         }
-        duplicatedNumbers[j++] = numbers[n-1];
-        // Changing original array
-        for (int i=0; i<j; i++){
-            numbers[i] = duplicatedNumbers[i];
+        temp[j++] = numbers[n-1];
+        //Changing original array
+        for(int i=0; i<j; i++){
+            numbers[i] = temp[i];
         }
         return j;
     }
 
 
 
-
-
-
-
     //Exercise 09
-        public static String[] addElements (String[]surNames, String elementToAdd){
+       /*public static String[] addElements (String[]surNames, String elementToAdd){
             surNames = Arrays.copyOf(surNames, surNames.length + 1);
             surNames[surNames.length - 1] = elementToAdd;
             return surNames;
-        }
+        }*/
+
+
 
 
         public static void main (String[]args){
+
+
+
 
         /*Exercise 01
         int[] numbers = {1,2,3,4,5};
@@ -124,19 +119,21 @@ public class WeekTwoExercises {
             }
         }*/
 
-            //Exercise 08 (INCOMPLETE)
-            int numbers[] = {20, 20, 40, 20, 30, 40, 50, 60, 50};
-            System.out.println("Array numbers: " + Arrays.toString(numbers));
+            /*Exercise 08
+            int[] numbers = {20, 20, 40, 20, 30, 40, 50, 60, 50};
             Arrays.sort(numbers);
+            System.out.println("Original Array Numbers, sorted with duplicates: " + Arrays.toString(numbers));
             int length = numbers.length;
-            length = removeDuplicateElements(numbers, length);
+            length = removeDuplicatedElements(numbers, length);
+            System.out.print("Array Numbers without duplicates: ");
             //printing array elements
-            for (int i=0; i<length; i++)
-                System.out.print(numbers[i] + " ");
+            for(int i=0; i<length; i++)
+                System.out.print(numbers[i] + " ");*/
+
 
 
             //Exercise 09
-        /*String[] surNames={"Robinson", "Davis", "Williams", "Jordan"};
+       /* String[] surNames={"Robinson", "Davis", "Williams", "Jordan"};
         String elementToAdd = "Elbrink";
         surNames = addElements(surNames, elementToAdd);
         elementToAdd = "Alcantara";
